bc082944512073a48197f68622069dadf21d0553
ashrafomid2025 <omidmashraf@gmail.com>
2025-03-28 08:56:51 +0430

    first commit

---
+++ public/app.js
@@ -0,0 +1,19 @@
+const horizontalNav = document.getElementById("hnav");
+const verticalNav = document.getElementById("vnav");
+const bars = document.getElementById("bars");
+const close = document.getElementById("close");
+bars.addEventListener("click",()=>{
+    horizontalNav.classList.add("w-[75%]");
+    horizontalNav.classList.remove("w-full");
+    verticalNav.classList.remove("hidden");
+    bars.style.display = "none";
+    close.style.display = "block";
+});
+close.addEventListener("click",()=>{
+    horizontalNav.classList.add("w-full");
+    horizontalNav.classList.remove("w-[75%]");
+    verticalNav.classList.add("hidden");
+    bars.style.display = "block";
+    close.style.display = "none";
+
+});
